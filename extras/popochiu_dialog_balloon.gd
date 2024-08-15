46d9c7b967d03d4ee068b1d279cdc27862ff9b2a
Anton Ivanov <me@antonivanov.net>
2024-08-16 00:08:37 +0300

    Reuse common processing logic

--- extras/popochiu_dialog_balloon.gd
+++ extras/popochiu_dialog_balloon.gd
@@ -7,13 +7,17 @@ extends PopochiuDialog
 
 #region Virtual ####################################################################################
 func _on_start() -> void:
-	var start_dialogue_line = await dialogue_resource.get_next_dialogue_line(start_title)
-	_process_dialogue_line(start_dialogue_line)
+	_process_next(start_title)
 
 
 #endregion
 
 #region Private ####################################################################################
+func _process_next(title: String) -> void:
+	var next_line = await dialogue_resource.get_next_dialogue_line(title)
+	_process_dialogue_line(next_line)
+
+
 func _process_dialogue_line(dialogue_line: DialogueLine) -> void:
 	var character = _get_speaking_character(dialogue_line.character)
 	if character == null:
@@ -29,11 +33,6 @@ func _process_dialogue_line(dialogue_line: DialogueLine) -> void:
 func _get_speaking_character(character_name: String) -> PopochiuCharacter:
 	var is_player_speaking = character_name.to_lower() == "player"
 	return C.player if is_player_speaking else C.get_runtime_character(character_name)
-
-
-func _process_next(next_id: String) -> void:
-	var next_dialogue_line = await dialogue_resource.get_next_dialogue_line(next_id)
-	_process_dialogue_line(next_dialogue_line)
 
 
 #endregion
