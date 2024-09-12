aa29a5985172694fc9e313c96425dd41c539757c
Paolo Pustorino <stickgrinder@gmail.com>
2024-09-12 08:39:14 +0200

    Added missing audio to the door prop.

--- game/autoloads/a.gd
+++ game/autoloads/a.gd
@@ -3,6 +3,7 @@ extends "res://addons/popochiu/engine/interfaces/i_audio.gd"
 
 # cues ----
 var mx_two_popochius: AudioCueMusic = load("res://game/rooms/house/audio/mx_two_popochius.tres")
+var sfx_door_open: AudioCueSound = load("res://game/rooms/house/props/door/sfx_door_open.tres")
 var sfx_apple_fall: AudioCueSound = load("res://game/rooms/outside/props/tree/sfx_apple_fall.tres")
 var sfx_tree_impact: AudioCueSound = load("res://game/rooms/outside/props/tree/sfx_tree_impact.tres")
 # ---- cues
