6d01dc1bf203830136b5cb65132b1a3f3e9ba170
carenalga <mapedorr@gmail.com>
2024-09-11 22:26:34 -0500

    Update Popochiu version with the fix for the TransitionLayer

--- game/autoloads/a.gd
+++ game/autoloads/a.gd
@@ -2,5 +2,8 @@
 extends "res://addons/popochiu/engine/interfaces/i_audio.gd"
 
 # cues ----
+var mx_two_popochius: AudioCueMusic = load("res://game/rooms/house/audio/mx_two_popochius.tres")
+var sfx_apple_fall: AudioCueSound = load("res://game/rooms/outside/props/tree/sfx_apple_fall.tres")
+var sfx_tree_impact: AudioCueSound = load("res://game/rooms/outside/props/tree/sfx_tree_impact.tres")
 # ---- cues
 
